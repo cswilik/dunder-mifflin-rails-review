15b1f6509a930d5e108b2379575cafb56de96808
therunningagent <luistjr@gmail.com>
2020-11-19 16:50:18 -0500

    finished lab

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end 
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+    
+    def create
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, :title, uniqueness: true
+
+    def full_name 
+        self.first_name + " " + self.last_name 
+    end 
+
+    # def is_alias_unique
+    # end 
 end
